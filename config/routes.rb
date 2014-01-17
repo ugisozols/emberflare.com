d69afe822b2408da2851ecaa4afc9813546ee035
Uģis Ozols <ugis.ozolss@gmail.com>
2014-01-17 11:40:24 +0200

    Use catch all route which uses static#index which in turn is handled by Ember.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ EmberFlare::Application.routes.draw do
   root :to => "static#index"
 
   namespace :api do
-    resources :entries
+    resources :entries, :only => [:index, :create]
     resource :session, :only => :create
   end
 
   post :session, :to => "session#create", :format => :json
   delete :session, :to => "session#destroy", :format => :json
+
+  get "/*path" => "static#index"
 end
