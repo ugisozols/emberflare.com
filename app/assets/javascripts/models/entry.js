3f5919db6442f80a837f2b1ff76f8fb0a67e3dba
Uģis Ozols <ugis.ozolss@gmail.com>
2014-02-14 11:59:54 +0200

    Show gravatar for logged in user and submitted entries.

--- app/assets/javascripts/models/entry.js
+++ app/assets/javascripts/models/entry.js
@@ -1,6 +1,7 @@
 EmberFlare.Entry = DS.Model.extend({
   title: DS.attr("string"),
   content: DS.attr("string"),
-  author: DS.attr("string"),
+  authorName: DS.attr("string"),
+  authorEmail: DS.attr("string"),
   createdAt: DS.attr("date")
 });
--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,11 +1,17 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :author, :created_at
+  attributes :id, :title, :content, :author_name, :author_email, :created_at
 
-  def author
-    if (user = object.user)
-      user.username
-    else
-      object.author
-    end
+  def author_name
+    user ? user.username : object.author
+  end
+
+  def author_email
+    user ? user.email : ""
+  end
+
+  private
+
+  def user
+    object.user
   end
 end
