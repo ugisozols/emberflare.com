218ad19d390b5e3fad9b20ca8cb44dbcad12eb9a
Uģis Ozols <ugis.ozolss@gmail.com>
2014-02-21 09:19:48 +0200

    If the user is already signed in the redirect to /entries when he tries to visit /signin.

--- app/assets/javascripts/routes/signin.js
+++ app/assets/javascripts/routes/signin.js
@@ -1,4 +1,10 @@
 EmberFlare.SigninRoute = Ember.Route.extend({
+  beforeModel: function() {
+    if (this.get("session.isAuthenticated")) {
+      this.transitionTo("entries");
+    }
+  },
+
   deactivate: function() {
     this.controllerFor("signin").set("signinFailed", false);
   }
