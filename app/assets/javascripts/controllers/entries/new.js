ac5f54984255d5a01f71d0418986a681630bec1c
Uģis Ozols <ugis.ozolss@gmail.com>
2014-03-06 11:21:35 +0200

    Add missing ;

--- app/assets/javascripts/controllers/entries/new.js
+++ app/assets/javascripts/controllers/entries/new.js
@@ -4,13 +4,12 @@ EmberFlare.EntriesNewController = Ember.ObjectController.extend({
   fieldsEmpty: function() {
     if (this.get("session").get("isAuthenticated")) {
       return Ember.isEmpty(this.get("title")) ||
-        Ember.isEmpty(this.get("content"))
+        Ember.isEmpty(this.get("content"));
     } else {
       return Ember.isEmpty(this.get("title")) ||
         Ember.isEmpty(this.get("content")) ||
-        Ember.isEmpty(this.get("authorName"))
+        Ember.isEmpty(this.get("authorName"));
     }
-
   }.property("title", "content", "authorName"),
 
   actions: {
