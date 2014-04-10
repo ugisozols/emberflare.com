a25eba8d39b77517600d8871b0f52aaff1994701
Uģis Ozols <ugis.ozolss@gmail.com>
2014-04-10 08:00:09 +0300

    Use the default page title for /entries.

--- app/assets/javascripts/routes/entries/index.js
+++ app/assets/javascripts/routes/entries/index.js
@@ -1,12 +1,5 @@
 EmberFlare.EntriesIndexRoute = Ember.Route.extend({
   model: function() {
     return this.store.find("entry");
-  },
-
-  actions: {
-    updateTitle: function(tokens) {
-      tokens.push("All entries");
-      return true;
-    }
   }
 });
