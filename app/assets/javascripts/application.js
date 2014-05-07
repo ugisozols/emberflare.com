f9ee43fc6d468a4157604d7058bed9e9d6f48697
Uģis Ozols <ugis.ozolss@gmail.com>
2014-05-07 15:44:45 +0300

    Forgot to include cookie store lib.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 //= require ember-data
 //= require ember-simple-auth
 //= require ember-simple-auth-oauth2
+//= require ember-simple-auth-cookie-store
 //= require ember-validations
 //= require moment.min
 //= require showdown.min
