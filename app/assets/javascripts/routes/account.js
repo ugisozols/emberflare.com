144b05a0b7cfd9cdb7eb10c144e3143064e0da54
Uģis Ozols <ugis.ozolss@gmail.com>
2014-06-09 13:26:41 +0300

    Put back code that was removed by mistake and make sure I don't brake it in the future.

--- app/assets/javascripts/routes/account.js
+++ app/assets/javascripts/routes/account.js
@@ -1,4 +1,4 @@
-EmberFlare.AccountRoute = Ember.Route.extend({
+EmberFlare.AccountRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
   model: function() {
     return this.store.find("account", "current_user");
   },
--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -1,28 +1,38 @@
 require "spec_helper"
 
 feature "account", :js => true do
-  before { sign_in }
+  context "when not signed in" do
+    it "redirects to sign in page" do
+      visit "/account"
 
-  context "when valid data" do
-    scenario "successful update" do
-      find('#account').click
-
-      fill_in :email, :with => "random@email.com"
-      click_button "Update"
-
-      expect(page).to have_content("Account was updated successfuly.")
-      expect(page).to have_selector("img[src*='#{Digest::MD5.hexdigest('random@email.com')}']")
+      expect(page.current_path).to eq("/signin")
     end
   end
 
-  context "when invalid data" do
-    scenario "failed update" do
-      find('#account').click
+  context "when signed in" do
+    before { sign_in }
 
-      fill_in :email, :with => ""
-      click_button "Update"
+    context "when valid data" do
+      scenario "successful update" do
+        find('#account').click
 
-      expect(page).to have_content("Account failed to update. Please make sure that the data you entered comforms to the rules mentioned below.")
+        fill_in :email, :with => "random@email.com"
+        click_button "Update"
+
+        expect(page).to have_content("Account was updated successfuly.")
+        expect(page).to have_selector("img[src*='#{Digest::MD5.hexdigest('random@email.com')}']")
+      end
+    end
+
+    context "when invalid data" do
+      scenario "failed update" do
+        find('#account').click
+
+        fill_in :email, :with => ""
+        click_button "Update"
+
+        expect(page).to have_content("Account failed to update. Please make sure that the data you entered comforms to the rules mentioned below.")
+      end
     end
   end
 end
